15e18e14a80006feab2e1ffbec61af556c4f4ec0
Joshua Equere <marveloper.8@gmail.com>
2022-01-13 19:19:47 +0100

    volume

--- 1.py
+++ 1.py
@@ -105,7 +105,20 @@
 # print(constants.mmHg)
 # print(constants.psi)
 
+# from scipy import constants
+
+# print(constants.hectare)
+# print(constants.acre)
+
 from scipy import constants
 
-print(constants.hectare)
-print(constants.acre)
+print(constants.liter)
+print(constants.litre)
+print(constants.gallon)
+print(constants.gallon_US)
+print(constants.gallon_imp)
+print(constants.fluid_ounce)
+print(constants.fluid_ounce_US)
+print(constants.fluid_ounce_imp)
+print(constants.barrel)
+print(constants.bbl)
