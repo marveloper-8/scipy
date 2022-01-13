dc80f9b72575309f851ecda60611d2cd02bd4faa
Joshua Equere <marveloper.8@gmail.com>
2022-01-13 19:16:26 +0100

    area

--- 1.py
+++ 1.py
@@ -96,11 +96,16 @@
 # print(constants.light_year)
 # print(constants.parsec)
 
+# from scipy import constants
+
+# print(constants.atm)
+# print(constants.atmosphere)
+# print(constants.bar)
+# print(constants.torr)
+# print(constants.mmHg)
+# print(constants.psi)
+
 from scipy import constants
 
-print(constants.atm)
-print(constants.atmosphere)
-print(constants.bar)
-print(constants.torr)
-print(constants.mmHg)
-print(constants.psi)
+print(constants.hectare)
+print(constants.acre)
