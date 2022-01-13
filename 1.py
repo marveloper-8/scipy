bc7c258052a52f5722126ee4621eb461ce12ffd9
Joshua Equere <marveloper.8@gmail.com>
2022-01-13 19:07:14 +0100

    length

--- 1.py
+++ 1.py
@@ -67,11 +67,31 @@
 # print(constants.arcsec)
 # print(constants.arcsecond)
 
+# from scipy import constants
+
+# print(constants.minute)
+# print(constants.hour)
+# print(constants.day)
+# print(constants.week)
+# print(constants.year)
+# print(constants.Julian_year)
+
 from scipy import constants
 
-print(constants.minute)
-print(constants.hour)
-print(constants.day)
-print(constants.week)
-print(constants.year)
-print(constants.Julian_year)
+print(constants.inch)
+print(constants.foot)
+print(constants.yard)
+print(constants.mile)
+print(constants.mil)
+print(constants.pt)
+print(constants.point)
+print(constants.survey_foot)
+print(constants.survey_mile)
+print(constants.nautical_mile)
+print(constants.fermi)
+print(constants.angstrom)
+print(constants.micron)
+print(constants.au)
+print(constants.astronomical_unit)
+print(constants.light_year)
+print(constants.parsec)
